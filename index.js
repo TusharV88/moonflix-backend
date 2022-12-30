29aa8112a0287eb3f02d055fa6918a3ca4c12f27
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-30 19:13:07 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import routes from './src/routes/index.js';
 
 const app = express();
 
-app.use(cors ({ origin: "https://moonflix.onrender.com", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
